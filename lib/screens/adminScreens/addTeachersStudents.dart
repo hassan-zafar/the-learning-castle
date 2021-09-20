4d65485c20e42d3dc5a8e6b91422c1d19e5b7474
Hassan Ayoub Zafar <hz.asd1@gmail.com>
2021-09-20 05:41:01 +0500

    added permissions in android manifest

--- lib/screens/adminScreens/addTeachersStudents.dart
+++ lib/screens/adminScreens/addTeachersStudents.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:glassmorphism_ui/glassmorphism_ui.dart';
-import 'package:the_learning_castle_v2/database/database.dart';
 import 'package:the_learning_castle_v2/services/authentication_service.dart';
 import 'package:the_learning_castle_v2/tools/custom_toast.dart';
 import 'package:the_learning_castle_v2/tools/loading.dart';
